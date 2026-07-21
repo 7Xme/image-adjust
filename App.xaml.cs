aa56438c143db56e2ac46cbba9dce3eb56d77d4c
7Xme <ayoubatigui33@gmail.com>
2026-07-21 17:55:04 +0100

    up

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,23 @@
+using System.Windows;
+
 namespace ImageAdjust
 {
-    public partial class App : System.Windows.Application
+    public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DispatcherUnhandledException += (_, args) =>
+            {
+                MessageBox.Show($"Error: {args.Exception.Message}\n\n{args.Exception.StackTrace}",
+                    "Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                args.Handled = true;
+            };
+            AppDomain.CurrentDomain.UnhandledException += (_, args) =>
+            {
+                MessageBox.Show($"Fatal: {args.ExceptionObject}",
+                    "Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+            base.OnStartup(e);
+        }
     }
 }
